d29508ae8ac70a95b31b585e88d848ba44aec1a7
unknown <ju.velling@gmail.com>
2024-12-15 02:22:41 +0100

    INIT

---
+++ lupus-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Lupus Plugin
+ * Description: Lupus Plugin is a plugin made for FC St. Pauli Werewolves.
+ * Version: 0.1
+ * Requires at least: 6.6.2
+ * Requires PHP: 8.1.29
+ * Author: Julian Velling
+ * Author URI: https://github.com/juve33
+ * License: MIT License
+ * License URI: https://github.com/juve33/lupus-plugin/blob/main/LICENSE
+ * Text Domain: lupus-plugin
+ * 
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
